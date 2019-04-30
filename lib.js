e4c86942f43dd77240c23fdf2680524dab05896c
sidvecious <lorenzo.olivier@libero.it>
2019-04-30 17:58:31 +0200

    first commit

---
+++ lib.js
@@ -0,0 +1,6 @@
+function say_hello()
+{
+	console.log("hello world");
+}
+
+module.exports = say_hello;
---
+++ prova.js
@@ -0,0 +1,9 @@
+//const myModule = require('./database_di_prova');
+//let users = myModule.databased();
+
+const users = require('./database_di_prova.js');
+
+databased.users();
+
+//users.push( {id: users.length+1, username: "tizio", password:"caio", age:36});
+console.log(users);
